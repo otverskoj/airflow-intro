ba5a3787238dd088bbbabc6d039cb8ae5603632b
Oleg Tverskoy <otverskoj@gmail.com>
2022-06-13 18:34:37 +0600

    Add simple API for sendind iris dataset samples

--- sender/sender.py
+++ sender/sender.py
@@ -1,40 +1,76 @@
-from time import sleep
-from typing import Union, Tuple
-from pprint import pprint
+# from time import sleep
+# from typing import Union, Tuple
+# from pprint import pprint
 
-from sklearn.datasets import load_iris
-import numpy as np
-import requests
+# from sklearn.datasets import load_iris
+# import numpy as np
+# import requests
 
-import config
-
-
-def send_sample(url: str, sample: dict[str, Union[float, int]]) -> None:
-    requests.post(url, sample)
+# import config
 
 
-def main() -> None:
-    iris = load_iris()
-    for sample in zip(iris.data, iris.target):
-        send_sample(
-            config.URL,
-            _format_sample(sample)
-            )
-        sleep(config.SLEEP_SECONDS)
+# def send_sample(url: str, sample: dict[str, Union[float, int]]) -> None:
+#     requests.post(url, sample)
 
 
-def _format_sample(
-    sample: Tuple[np.ndarray, str]
-) -> dict[str, Union[float, int]]:
-    data, target = sample
+# def main() -> None:
+#     iris = load_iris()
+#     for sample in zip(iris.data, iris.target):
+#         send_sample(
+#             config.URL,
+#             _format_sample(sample)
+#             )
+#         sleep(config.SLEEP_SECONDS)
+
+
+# def _format_sample(
+#     sample: Tuple[np.ndarray, str]
+# ) -> dict[str, Union[float, int]]:
+#     data, target = sample
+#     return {
+#         'sepal_length': data[0],
+#         'sepal_width': data[1],
+#         'petal_length': data[2],
+#         'petal_width': data[3],
+#         'target': target
+#     }
+
+
+# if __name__ == '__main__':
+#     main()
+
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from numpy import int32, float64
+
+from utils import prepare_iris
+
+
+app = FastAPI()
+
+
+IRIS_DATASET = prepare_iris()
+CURRENT_IRIS_SAMPLE_INDEX = 0
+
+
+class IrisDatasetSample(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+    target: int
+
+
+@app.get('/iris_sample', response_model=IrisDatasetSample)
+def iris_sample():
+    global CURRENT_IRIS_SAMPLE_INDEX
+    data = IRIS_DATASET[CURRENT_IRIS_SAMPLE_INDEX]
+    CURRENT_IRIS_SAMPLE_INDEX += 1
     return {
         'sepal_length': data[0],
         'sepal_width': data[1],
         'petal_length': data[2],
         'petal_width': data[3],
-        'target': target
+        'target': data[4]
     }
-
-
-if __name__ == '__main__':
-    main()
