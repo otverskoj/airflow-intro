1bae92ac81d3e6483b5126bc83b817c2c8360322
Oleg Tverskoy <otverskoj@gmail.com>
2022-06-14 14:20:09 +0600

    Change dir name in sender service

---
+++ sender/app/utils.py
@@ -0,0 +1,13 @@
+from sklearn.datasets import load_iris
+import numpy as np
+
+
+def prepare_iris():
+    iris = load_iris()
+    dataset = np.hstack((iris.data, iris.target[:, np.newaxis]))
+    np.random.shuffle(dataset)
+    return dataset
+
+
+if __name__ == '__main__':
+    prepare_iris()
